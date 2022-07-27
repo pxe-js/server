9a1124c3885edec6c9a74d02907cfe75aea16b3b
Revenity <87530663+aquapi@users.noreply.github.com>
2022-07-27 08:31:16 +0700

    Add bench directory

---
+++ bench/index.js
@@ -0,0 +1,28 @@
+const Server = require("..");
+
+const regx = /^\/user\/(?<id>[0-9]+)/i;
+
+const app = new Server();
+
+app.set("disable cookie", true);
+
+app.use(async ctx => {
+    const url = ctx.request.url;
+    const res = ctx.response;
+    const req = ctx.request;
+    
+    ctx.options.finishResponse = () => res.raw.end(res.body || "");
+
+    if ((req.method === "POST" && url === "/user") || (req.method === "GET" && url === "/"))
+        return;
+     
+    const id = regx.exec(url);
+    if (id) {
+        ctx.response.body = id.groups.id;
+        return;
+    }
+    
+    ctx.response.raw.statusCode = 404;
+});
+
+app.ls(8080);
