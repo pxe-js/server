943985a559231d0caa520c6be661470f788f89d1
Revenity <87530663+aquapi@users.noreply.github.com>
2022-09-11 12:57:11 +0700

    [1.0.13] Group all benchmarking utils into one file

--- bench/run.js
+++ bench/run.js
@@ -1,18 +1,29 @@
-const { exec } = require("child_process");
-const { writeFile } = require("fs/promises");
-const osDetail = require("./osDetail");
+// Get OS details
+const os = require('os');
 
-/**
- * @type {import("autocannon").Options}
- */
-const options = {
-    connections: 64,
-    duration: 15,
-    workers: 8,
-    timeout: 8
+const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+function convertBytes(x) {
+    let l = 0, n = parseInt(x, 10) || 0;
+
+    while (n >= 1024 && ++l)
+        n /= 1024;
+
+    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + units[l]);
 }
 
-function parseOptions() {
+function getCPU() {
+    const cpus = os.cpus();
+
+    return cpus.length + " Cores (" + cpus[0].model + ")";
+};
+
+function getDetails() {
+    return "CPU: " + getCPU() + "\n" + "RAM: " + convertBytes(os.totalmem()) + "\n" + "OS: " + os.type() + "\n\n";
+}
+
+// Utils
+function parseOptions(options) {
     let str = "";
 
     for (const key in options)
@@ -25,9 +36,16 @@ function escapeColor(text) {
     return text.replaceAll(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, "");
 }
 
+// Benchmarking
+const { exec } = require("child_process");
+const { writeFile, readFile } = require("fs/promises");
+
 async function bench() {
     console.log("Start benchmarking...");
-    const proc = exec("npx autocannon http://localhost:3000 " + parseOptions());
+    const proc = exec("npx autocannon http://localhost:3000 " + parseOptions(
+        await readFile("./bench/config.json")
+            .then(t => JSON.parse(t.toString()))
+    ));
 
     let data = "";
 
@@ -35,7 +53,7 @@ async function bench() {
 
     proc.on("exit", () => {
         console.log("Writing result to file...");
-        writeFile("./bench/result.txt", osDetail + escapeColor(data))
+        writeFile("./bench/result.txt", getDetails() + escapeColor(data))
             .then(() => {
                 console.log("Done!");
                 process.exit();
