b15e3e3bf3ddd740a128d7d7fd20463606a20d29
Revenity <87530663+aquapi@users.noreply.github.com>
2022-09-11 08:24:31 +0700

    [1.0.13] Added more details

--- bench/run.js
+++ bench/run.js
@@ -1,10 +1,18 @@
 const autocannon = require("autocannon");
 const { createWriteStream } = require("fs");
+const os = require("os");
+const convert = require("./convert");
 const options = {
     connections: 64,
     duration: 15,
     workers: 8,
     timeout: 8
+}
+
+function getCPU() {
+    const cpus = os.cpus();
+
+    return cpus.length + " Cores (" + cpus[0].model + ")";
 }
 
 async function bench() {
@@ -14,9 +22,15 @@ async function bench() {
     });
 
     const wstr = createWriteStream("./bench/result.txt");
+
+    wstr.write("Platform: " + os.type() + "\n");
+    wstr.write("RAM: " + convert(os.totalmem()) + "\n");
+    wstr.write("CPU: " + getCPU() + "\n\n");
+
     wstr.write("Average Req/sec: " + res.requests.average + "\n");
     wstr.write("Max Req/sec: " + res.requests.max + "\n");
     wstr.write("Total Req/sec: " + res.requests.total);
+
     wstr.end();
     wstr.close(() => process.exit());
 }
